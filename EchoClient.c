a467d75accfe3793666c582a128f80744a270dc0
hyoeun <hhyy0303@naver.com>
2019-10-01 16:42:20 +0900

    EchoServer 수정 : quit명령어 구현(4-2)

--- EchoClient.c
+++ EchoClient.c
@@ -35,7 +35,9 @@ int main(){
 		fgets(sendBuffer, sizeof(sendBuffer),stdin);//stdin을 사용해서 키보드로 메세지를 입력받을 수 있다.
 		//입력받은 메세지 서버로 보내기
 		write(c_socket, sendBuffer, strlen(sendBuffer));
-
+		//입력받은 메세지가 quit 이면 break
+		if(strncasecmp(sendBuffer, "quit", 4) == 0)
+			break;
 		//5. 서버에서 보낸 메시지 읽기 
 		n = read(c_socket, rcvBuffer, sizeof(rcvBuffer)); 
 		//서버에서 보내준 메세지를 rcvBuffer에 저장하고, 메세지의 길이를 리턴
